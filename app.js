3a243d3041697f72e39fd72bce015c0d0b1dffd3
Pratyasha955 <pratyashasatapathy79@gmail.com>
2023-10-23 16:29:54 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+//getElementById
+var page=document.getElementById("main-header");
+page.style.borderBottom="solid 3px #000";
+var pagelog=document.getElementById("text");
+pagelog.style.fontWeight = 'bold';
+pagelog.style.color = 'green';
