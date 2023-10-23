0397047bfd83c7720cfc2a6339a71fd51f58a337
Pratyasha955 <pratyashasatapathy79@gmail.com>
2023-10-23 20:46:57 +0530

    secondcommit

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-//getElementById
-var page=document.getElementById("main-header");
-page.style.borderBottom="solid 3px #000";
-var pagelog=document.getElementById("text");
-pagelog.style.fontWeight = 'bold';
-pagelog.style.color = 'green';
+// //getElementById
+// var page=document.getElementById("main-header");
+// page.style.borderBottom="solid 3px #000";
+// var pagelog=document.getElementById("text");
+// pagelog.style.fontWeight = 'bold';
+// pagelog.style.color = 'green';
+
+//getElementByClassName
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].style.backgroundColor ='green';
+items[2].textContent ='Hello';
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight ='bold';
+}
