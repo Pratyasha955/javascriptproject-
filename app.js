879f5aa73eecc80fa0ec0cf8a300130746b0d405
Pratyasha955 <pratyashasatapathy79@gmail.com>
2023-10-23 21:50:22 +0530

    fourth commit

--- app.js
+++ app.js
@@ -20,11 +20,27 @@
 // items1[0].style.backgroundColor='yellow';
 // items1[0].style.fontWeight='bold';
 
-//getelementbytagname
-var items2=document.getElementsByTagName("li");
-console.log(items2);
-items2[0].textContent="Hello";
-items2[1].style.backgroundColor='green';
-for(let i=0;i<items2.length;i++){
-    items2[i].style.fontWeight='bold';
+// //getelementbytagname
+// var items2=document.getElementsByTagName("li");
+// console.log(items2);
+// items2[0].textContent="Hello";
+// items2[1].style.backgroundColor='green';
+// for(let i=0;i<items2.length;i++){
+//     items2[i].style.fontWeight='bold';
+// }
+
+// //QuerySelector 
+
+// var iteams3=document.querySelector('.list-group-item:nth-child(2)');
+// iteams3.style.backgroundColor='green';
+
+// var iteams4=document.querySelector('.list-group-item:nth-child(3)');
+// iteams4.style.display='none';
+
+//QuerySelectorALL
+var titels1=document.querySelectorAll('li');
+titels1[1].style.color='green';
+var titels2=document.querySelectorAll('.list-group-item:nth-child(odd)');
+for(let i=0;i<titels2.length;i++){
+titels2[i].style.backgroundColor='green';
 }
