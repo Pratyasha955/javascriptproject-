27c5f44eba4dec1688be4ba1616cbf92f5923c03
Pratyasha955 <pratyashasatapathy79@gmail.com>
2023-10-23 21:11:19 +0530

    thirdcommit

--- app.js
+++ app.js
@@ -5,13 +5,26 @@
 // pagelog.style.fontWeight = 'bold';
 // pagelog.style.color = 'green';
 
-//getElementByClassName
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[2]);
-items[2].style.backgroundColor ='green';
-items[2].textContent ='Hello';
+// //getElementByClassName
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[2]);
+// items[2].style.backgroundColor ='green';
+// items[2].textContent ='Hello';
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight ='bold';
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight ='bold';
+// }
+// var  items1= document.getElementsByClassName('list');
+// console.log(items1);
+// items1[0].style.backgroundColor='yellow';
+// items1[0].style.fontWeight='bold';
+
+//getelementbytagname
+var items2=document.getElementsByTagName("li");
+console.log(items2);
+items2[0].textContent="Hello";
+items2[1].style.backgroundColor='green';
+for(let i=0;i<items2.length;i++){
+    items2[i].style.fontWeight='bold';
 }
