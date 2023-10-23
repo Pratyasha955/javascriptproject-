e9d54534e2221a9ca18d517d5d5cdbb986540c91
Pratyasha955 <pratyashasatapathy79@gmail.com>
2023-10-24 01:23:40 +0530

    fifth commit

--- app.js
+++ app.js
@@ -37,10 +37,69 @@
 // var iteams4=document.querySelector('.list-group-item:nth-child(3)');
 // iteams4.style.display='none';
 
-//QuerySelectorALL
-var titels1=document.querySelectorAll('li');
-titels1[1].style.color='green';
-var titels2=document.querySelectorAll('.list-group-item:nth-child(odd)');
-for(let i=0;i<titels2.length;i++){
-titels2[i].style.backgroundColor='green';
-}
+// //QuerySelectorALL
+// var titels1=document.querySelectorAll('li');
+// titels1[1].style.color='green';
+// var titels2=document.querySelectorAll('.list-group-item:nth-child(odd)');
+// for(let i=0;i<titels2.length;i++){
+// titels2[i].style.backgroundColor='green';
+// }
+
+// 
+
+
+// //par//TRAVERSING THE DOM
+// var itemList = document.querySelector('#items');anetNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = 'lightgreen';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// //ParentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = 'yellow';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// //childeNodes
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'lightpink';
+
+// //FirstChild
+// console.log(itemList.firstChild);
+// //FirstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'AddHello';
+
+// //nextsibling
+// console.log(itemList.nextSibling);
+// //nextelementsibling
+// console.log(itemList.nextElementSibling);
+
+// //previous
+ //console.log(itemList.previous);
+// //previousElementSibling
+//console.log(itemList.previousElementSibling);
+ //itemList.previousElementSibling.style.color = 'green';
+
+
+ //createElement
+ var newDiv = document.createElement('div');
+ // Add Class
+ newDiv.className = 'hiipooja';
+ //Add Id
+ newDiv.id ='hiiashu' ;
+ //Add attribute
+ newDiv.setAttribute ('tittle','hello div');
+// create text node
+var newDivText = document.createTextNode('ADDHello');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv, h1);
